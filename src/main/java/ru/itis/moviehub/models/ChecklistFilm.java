403ab4d016d9c83d8010d9bfc37a936b13244085
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-04-17 09:51:32 +0300

    lil fixes in model added

--- src/main/java/ru/itis/moviehub/models/ChecklistFilm.java
+++ src/main/java/ru/itis/moviehub/models/ChecklistFilm.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -18,6 +15,8 @@ import javax.persistence.Table;
 @Table(name = "checklist_film")
 public class ChecklistFilm {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long checklist;
     @Column(name = "checklist_id")
     private Long id;
     @Column(name = "film_id")
