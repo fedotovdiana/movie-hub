d43736812083d3b3146e395ca36d42415dda21b2
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-05-01 12:20:43 +0300

    sockets fixed

--- src/main/java/ru/itis/moviehub/handlers/WebSocketMessagesHandler.java
+++ src/main/java/ru/itis/moviehub/handlers/WebSocketMessagesHandler.java
@@ -3,11 +3,11 @@ package ru.itis.moviehub.handlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import ru.itis.moviehub.dto.Message;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,18 +23,24 @@ public class WebSocketMessagesHandler extends TextWebSocketHandler {
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        String messageText = (String) message.getPayload();
-        Message messageFromJson = objectMapper.readValue(messageText, Message.class);
-        System.out.println("QWERTY@@@@@@@@");
+//        String messageText = (String) message.getPayload();
+//        Message messageFromJson = objectMapper.readValue(messageText, Message.class);
+        System.out.println("handleMessage");
         for (WebSocketSession currentSession : sessions.values()) {
             currentSession.sendMessage(message);
-            System.out.println("QWERTY!!!!!!!!!!!!!!1");
+            System.out.println(" currentSession");
         }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("QWERTY");
+        System.out.println("afterConnectionEstablished");
         sessions.put(session.getId(), session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("afterConnectionClosed");
+        sessions.remove(session.getId());
     }
 }
