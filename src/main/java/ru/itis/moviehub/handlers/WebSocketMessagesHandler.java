e64020b1be0088d42f4a039abba59adef4bd8e68
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-05-01 13:46:20 +0300

    chat.ftlh fixed

--- src/main/java/ru/itis/moviehub/handlers/WebSocketMessagesHandler.java
+++ src/main/java/ru/itis/moviehub/handlers/WebSocketMessagesHandler.java
@@ -1,7 +1,5 @@
 package ru.itis.moviehub.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
@@ -18,29 +16,21 @@ public class WebSocketMessagesHandler extends TextWebSocketHandler {
 
     private static final Map<String, WebSocketSession> sessions = new HashMap<>();
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-//        String messageText = (String) message.getPayload();
-//        Message messageFromJson = objectMapper.readValue(messageText, Message.class);
         System.out.println("handleMessage");
         for (WebSocketSession currentSession : sessions.values()) {
             currentSession.sendMessage(message);
-            System.out.println(" currentSession");
         }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("afterConnectionEstablished");
         sessions.put(session.getId(), session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("afterConnectionClosed");
         sessions.remove(session.getId());
     }
 }
